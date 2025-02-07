212ebaebf0efcd8a60c6e88b9ea20822e201412d
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-02-08 03:22:11 +0800

    WIP: reinitialize api setup

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResouce('customers', UserController::class);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
