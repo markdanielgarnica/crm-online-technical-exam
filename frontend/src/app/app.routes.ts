5fa93c95251f40c3dab723ec837aa364017598cd
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-02-08 10:13:50 +0800

    WIP: setup customer crud routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CustomerListComponent } from './pages/customer-list/customer-list.component';
+import { CustomerCreateComponent } from './pages/customer-create/customer-create.component';
+import { CustomerViewComponent } from './pages/customer-view/customer-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CustomerListComponent },
+  { path: 'customer/create', component: CustomerCreateComponent },
+  { path: 'customer/:id', component: CustomerViewComponent },
+];
