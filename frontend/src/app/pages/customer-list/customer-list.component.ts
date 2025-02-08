f5f00bf945737946d4a6f6c30e60c7e74055ab28
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-02-08 11:25:54 +0800

    chore: imported RouterModule

--- frontend/src/app/pages/customer-list/customer-list.component.ts
+++ frontend/src/app/pages/customer-list/customer-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-customer-list',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './customer-list.component.html',
-  styleUrl: './customer-list.component.css'
+  styleUrl: './customer-list.component.css',
 })
-export class CustomerListComponent {
-
-}
+export class CustomerListComponent {}
