324418a11922ac8f0565b6115fbe7aee8e8e6c64
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-02-08 13:40:12 +0800

    WIP: init setup for customer view page

--- frontend/src/app/pages/customer-view/customer-view.component.ts
+++ frontend/src/app/pages/customer-view/customer-view.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-customer-view',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './customer-view.component.html',
-  styleUrl: './customer-view.component.css'
+  styleUrl: './customer-view.component.css',
 })
-export class CustomerViewComponent {
-
-}
+export class CustomerViewComponent {}
