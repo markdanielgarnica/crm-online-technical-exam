e74691f2529626fa13b62269d5725b7bc16518ef
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-02-08 09:33:38 +0800

    WIP: initial setup for customer view page

---
+++ frontend/src/app/pages/customer-view/customer-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-view',
+  imports: [],
+  templateUrl: './customer-view.component.html',
+  styleUrl: './customer-view.component.css'
+})
+export class CustomerViewComponent {
+
+}
