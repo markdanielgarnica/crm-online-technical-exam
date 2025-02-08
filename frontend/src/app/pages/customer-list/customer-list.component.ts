4827088d034bdc09f2e4388b91860881f1154b75
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-02-08 11:53:18 +0800

    feat: add list customer

--- frontend/src/app/pages/customer-list/customer-list.component.ts
+++ frontend/src/app/pages/customer-list/customer-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-customer-list',
-  imports: [RouterModule],
+  imports: [RouterModule, NgFor],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css',
 })
-export class CustomerListComponent {}
+export class CustomerListComponent {
+  customers: any[] = [];
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit(): void {
+    this.loadCustomers();
+  }
+
+  loadCustomers() {
+    this.customerService.getCustomers().subscribe((data) => {
+      this.customers = data;
+    });
+  }
+}
